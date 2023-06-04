04999fb2de0a6b1aa6a63245a4fe9be5f022454e
dugenou <frank.maestre@gmail.com>
2023-06-04 14:03:35 +0200

    creation of mongodb functions: connect, list ,select , create new collection, list collection

--- data_normalisation/data_normalisation.py
+++ data_normalisation/data_normalisation.py
@@ -2,6 +2,7 @@ from urllib import request
 import gzip
 import zipfile
 import json
+import os
 from pymongo import MongoClient
 
 ################ Function definition area #####################################
@@ -27,37 +28,89 @@ def extract(filename):
 
     print("unzipped")
 
+def find_json_files(folder_path):
+    json_files = []
 
-# function to establish connection to Mongodb database
+    # Iterate over all files and folders in the given folder path
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            # Check if the file has a .json extension
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                json_files.append(file_path)
+    print(json_files)
+
+    return json_files
+
+# function to establish connection to Mongodb database, and
 def connect_mongodb(host,port):
     client = MongoClient(host = host, port = port)
+    return client
+
+def list_of_database(client):
     print(client.list_database_names())
 
-# function to mongoimport data
-def mongo_import():
+def select_database(client,database):
+    database = client[database]
+    return database
+
+# function to create a new collection in mongodb
+def create_collection(client,database_name, collection):
+    database = client[database_name]
+    database.create_collection(name=collection)
+
+def list_collection(client,database_name):
+    database = client[database_name]
+    collections = database.list_collections()
+    for collection in collections:
+        print(collection)
+
+def select_collection(client,database,collection):
+    database = client[database]
+    collection = database[collection]
+    print("selected collection:", collection.name)
+    return collection
+
+# function to load json files to mongodb
+def load_mongodb(collection, json_files):
+    for file in json_files:
+        with open(file) as f:
+            data = json.load(f)
+    print(data)
+
 
 ############################## Main area ########################################
 
 host = '127.0.0.1'
-#zu789 = 12.23.45.12
+
 # download json file from datatourisme (.gz)
 download_archive(url = "https://diffuseur.datatourisme.fr/webservice/88bb302ae883e577743cce4f0f793282/b09342b4-4114-4c68-9ece-e9bcc36c650e")
 
 # gunzip + unzip archive
 extract("all_data.gz")
 
-#normalize json to dataframe
-#dict = json.loads("data/objects/0/02/13-022e6234-4c65-3e23-9938-f31ac9ec9bbb.json")
-#dict = json.loads(data)
-#df2 = json_normalize(dict['technologies'])
-#print(df2)
+# recurslively parse folder for json files
+json_files = find_json_files('data/objects')
 
-# Load the JSON data
-with open("data/objects/0/02/13-022e6234-4c65-3e23-9938-f31ac9ec9bbb.json") as f:
-    data = json.load(f)
-print(data)
+# connect to mongodb
+client = connect_mongodb(host,27017)
 
-connect_mongodb(host,27017)
+# list database name from mongodb
+list_of_database(client)
+
+# select database in mongodb
+database = select_database(client,'itineraire')
+
+# create new collection
+create_collection(client,database.name,'point_interest')
+
+# list collection from mongodb (in currently selected database)
+list_collection(client,database.name)
+
+# select collection from mongodb database
+collection = select_collection(client,'itineraire','point_interest')
+
+# Load json data to mongodb to poi
+#load_mongodb(collection, json_files)
 
 
-#df = pd.read_json("data/objects/0/02/13-022e6234-4c65-3e23-9938-f31ac9ec9bbb.json",orient="rdfs:comment")
