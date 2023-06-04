8da937786c4885d87e99755b5f7d7968fe682d8f
dugenou <frank.maestre@gmail.com>
2023-06-04 09:24:50 +0200

    established communication to Mongodb with underlying ssh -L 27017:172.18.0.2:27017 zu789  to docker image in digital ocean

--- data_normalisation/data_normalisation.py
+++ data_normalisation/data_normalisation.py
@@ -1,13 +1,16 @@
 from urllib import request
 import gzip
 import zipfile
-import pandas as pd
 import json
-from pandas import json_normalize
+from pymongo import MongoClient
 
+################ Function definition area #####################################
+
+# function to download archive from a Url in parameter
 def download_archive(url):
     request.urlretrieve(url, "data/all_data.gz")
 
+# function to gunzip and unzip an archive
 def extract(filename):
     # gunzip .gz
     input = gzip.GzipFile("data/"+filename, 'rb')
@@ -25,8 +28,36 @@ def extract(filename):
     print("unzipped")
 
 
+# function to establish connection to Mongodb database
+def connect_mongodb(host,port):
+    client = MongoClient(host = host, port = port)
+    print(client.list_database_names())
+
+# function to mongoimport data
+def mongo_import():
+
+############################## Main area ########################################
+
+host = '127.0.0.1'
+#zu789 = 12.23.45.12
 # download json file from datatourisme (.gz)
 download_archive(url = "https://diffuseur.datatourisme.fr/webservice/88bb302ae883e577743cce4f0f793282/b09342b4-4114-4c68-9ece-e9bcc36c650e")
 
 # gunzip + unzip archive
 extract("all_data.gz")
+
+#normalize json to dataframe
+#dict = json.loads("data/objects/0/02/13-022e6234-4c65-3e23-9938-f31ac9ec9bbb.json")
+#dict = json.loads(data)
+#df2 = json_normalize(dict['technologies'])
+#print(df2)
+
+# Load the JSON data
+with open("data/objects/0/02/13-022e6234-4c65-3e23-9938-f31ac9ec9bbb.json") as f:
+    data = json.load(f)
+print(data)
+
+connect_mongodb(host,27017)
+
+
+#df = pd.read_json("data/objects/0/02/13-022e6234-4c65-3e23-9938-f31ac9ec9bbb.json",orient="rdfs:comment")
