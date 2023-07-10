d1d3af536350dd16fa4f326cd7988840b8d2a70c
dugenou <frank.maestre@gmail.com>
2023-07-10 09:42:44 +0200

    sqlalchemy added students table with foreign_kez reference + PL/SQL drop script

--- sql_alchemy_test/connect_oracle.py
+++ sql_alchemy_test/connect_oracle.py
@@ -4,26 +4,47 @@ from sqlalchemy import Table, Column, Integer, String, ForeignKey, MetaData, cre
 engine = create_engine('oracle+cx_oracle://sys:Mafadu120c@192.168.80.2:1521/ORCLCDB?mode=sysdba',echo=True)
 
 #sql_drop_parcours = text('DROP TABLE IF EXISTS parcours')
-sql_drop_parcours = """
+sql_drop_students = """
 BEGIN
-   EXECUTE IMMEDIATE 'DROP TABLE parcours';
-EXCEPTION
-   WHEN OTHERS THEN
-      IF SQLCODE != -942 THEN
-         RAISE;
-      END IF;
-END;
+       EXECUTE IMMEDIATE 'DROP TABLE students';
+    EXCEPTION
+       WHEN OTHERS THEN
+          IF SQLCODE != -942 THEN
+             RAISE;
+          END IF;
+    END;
 """
+
+sql_drop_parcours = """
+    BEGIN
+       EXECUTE IMMEDIATE 'DROP TABLE parcours';
+    EXCEPTION
+       WHEN OTHERS THEN
+          IF SQLCODE != -942 THEN
+             RAISE;
+          END IF;
+    END;
+"""
+
 with engine.connect() as conn:
-   result = conn.execute(text(sql_drop_parcours))
+    result_drop_students = conn.execute(text(sql_drop_students))
+    result_drop_parcours = conn.execute(text(sql_drop_parcours))
 print(result)
 
 meta = MetaData()
 
 parcours = Table(
-   'parcours', meta,
-   Column('id', Integer, primary_key=True),
-   Column('name', String(length=100))
+    'parcours', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(length=100))
+)
+
+students = Table(
+    'students', meta,
+    Column('id', Integer, primary_key=True),
+    Column('firstname', String(length=100)),
+    Column('lastname', String(length=100)),
+    Column('parcours_id', Integer, ForeignKey("parcours.id")),
 )
 
 meta.create_all(engine)
@@ -37,8 +58,10 @@ with engine.connect() as conn:
 inspector = inspect(engine)
 
 tables_name = inspector.get_table_names()
-print(tables_name)
+print("Nom des tables:",tables_name)
 
 columns_name=inspector.get_columns(table_name='parcours')
-print(columns_name)
+print("Nom des colonnes de la table parcours:",columns_name)
+
+
 
